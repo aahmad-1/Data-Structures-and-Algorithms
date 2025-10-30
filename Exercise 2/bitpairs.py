7e7ecddd4b56c7b54d31f014c76cf4482c130780
aahmad-1 <aahmad.paturusii@gmail.com>
2025-10-30 21:59:16 +0200

    .

--- Exercise 2/bitpairs.py
+++ Exercise 2/bitpairs.py
@@ -1,20 +1,23 @@
 def pairs(s):
-    count_ones = 0       # number of 1s seen so far
-    sum_indices = 0      # sum of indices of previous 1s
-    total = 0            # final answer
-
-    i = 0
-    while i < len(s):
+    total = 0
+    count = 0  # number of '1's seen so far
+    sum_positions = 0  # sum of positions of all '1's seen so far
+    
+    for i in range(len(s)):
         if s[i] == '1':
-            total += count_ones * i - sum_indices
-            count_ones += 1
-            sum_indices += i
-        i += 1
-
+            # This '1' at position i contributes:
+            # (i * count) - sum_positions
+            # which is the sum of distances from position i to all previous '1's
+            total += i * count - sum_positions
+            
+            # Update for next iteration
+            count += 1
+            sum_positions += i
+    
     return total
 
 
 if __name__ == "__main__":
-    print(pairs("100101"))      
-    print(pairs("101"))         
-    print(pairs("100100111001")) 
+    print(pairs("100101"))
+    print(pairs("101"))
+    print(pairs("100100111001"))
